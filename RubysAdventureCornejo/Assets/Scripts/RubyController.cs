904ecba3108c3fb46e53c509a83df4ffc1df440f
heissy <heisycornejo@gmail.com>
2022-12-02 09:54:37 -0800

    Scirpts
    Added scripts to Ruby

--- RubysAdventureCornejo/Assets/Scripts/RubyController.cs
+++ RubysAdventureCornejo/Assets/Scripts/RubyController.cs
@@ -4,28 +4,43 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    public float speed = 3.0f;
+
+    public int maxHealth = 5;
+    int currentHealth;
+
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody2d = GetComponent<Rigidbody2D>();
+
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Create 2 variables to use the unity built in axes
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+         horizontal = Input.GetAxis("Horizontal");
+         vertical = Input.GetAxis("Vertical");
+    }
 
-        //Create our movement vector
-        Vector2 position = transform.position;
-
-        //Create horizontal and vertical movement
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
         position.x = position.x + 3.0f * horizontal * Time.deltaTime;
         position.y = position.y + 3.0f * vertical * Time.deltaTime;
 
-        //setting the new position
-        transform.position = position;
-        
+        rigidbody2d.MovePosition(position);
+    }
+
+    void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
